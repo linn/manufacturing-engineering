d8160eac86512b076b207795d5f9171580e01ff0
Lewis <lewis.renfrew@linn.co.uk>
2025-12-01 15:33:10 +0000

    redundant awscli install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -ev
 
-echo "Installing AWS CLI..."
-curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -q awscliv2.zip
-sudo ./aws/install >/dev/null 2>&1
-echo "AWS CLI installed."
+echo "Checking AWS CLI version..."
+aws --version
 
 # deploy on aws
 # Handle GitHub Actions environment variables
